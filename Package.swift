83411082a29344da17493d76ea91d339cd0fa3e0
truongdv <truongdv@sapo.vn>
2025-07-11 15:08:22 +0700

    v6.4.1

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "FreshchatSDK",
-                      url: "https://github.com/Truong207/freshchat-ios/releases/download/v6.4.0/FreshchatSDK.xcframework.zip",
+                      url: "https://github.com/Truong207/freshchat-ios/releases/download/v6.4.1/FreshchatSDK.xcframework.zip",
                       checksum: "8093e3e4f67c4f948c918080aab0aa63c3bfbcae64f180c622d961f152349d96")
     ]
 )
